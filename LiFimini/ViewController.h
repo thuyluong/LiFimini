fd702d006b1f815ddde1b213e435a600019e17cb
ThuyLuong <thuyluong@ThuyLuongs-MacBook-Pro.local>
2015-01-14 22:33:02 +0700

    Init project

---
+++ LiFimini/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiFimini
+//
+//  Created by ThuyLuong on 1/14/15.
+//  Copyright (c) 2015 ThuyLuong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LiFiminiTests/LiFiminiTests.m
@@ -0,0 +1,40 @@
+//
+//  LiFiminiTests.m
+//  LiFiminiTests
+//
+//  Created by ThuyLuong on 1/14/15.
+//  Copyright (c) 2015 ThuyLuong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface LiFiminiTests : XCTestCase
+
+@end
+
+@implementation LiFiminiTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
